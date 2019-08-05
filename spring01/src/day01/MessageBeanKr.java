f25bb977cb34fc76b1be241cedf8f626c0dead25
sist <sist@DESKTOP-DR0AI4H>
2019-08-05 09:25:21 +0900

    ddd

---
+++ spring01/src/day01/MessageBeanKr.java
@@ -0,0 +1,8 @@
+package day01;
+
+public class MessageBeanKr implements MessageBean{
+	public void sayHello(String name) {
+		System.out.println("안녕하세요, " + name + "님!");
+		System.out.println("왜여");
+	}
+}
